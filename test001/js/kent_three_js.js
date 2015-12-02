dd3b686b387e6028ee0f948f2a976d6429062832
Kent Chang <kwchang@jl-10-73-11-130.scad.edu>
2015-12-02 11:47:12 -0500

    el: impelemented shader structure

--- test001/js/kent_three_js.js
+++ test001/js/kent_three_js.js
@@ -5,7 +5,7 @@ init();
 render();
 
 function init(){
-// basis
+// basis info
 	var info = document.createElement( 'div' );
 	info.style.position = 'absolute';
 	info.style.top = '10px';
@@ -15,151 +15,57 @@ function init(){
 	info.style.link = '#f80';
 	info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> kent&rsquo;s test';
 	document.body.appendChild( info );
-
+// Renderer
 	renderer = new THREE.WebGLRenderer();
 	renderer.setClearColor( 0x222222 );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
-
-	// Stats
+// Shaders
+	var shaderTest = {
+		uniforms: {
+			color_uni: {
+				type: "v4",
+				value: new THREE.Vector4(0.34, 0.87, 0.6, 1.0)
+			}
+		},
+		vertexShader: document.getElementById("vs").textContent,
+		fragmentShader: document.getElementById("fs").textContent
+	}
+// Stats (Framerate)
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
 	stats.domElement.style.top = '0px';
 	info.appendChild( stats.domElement );
 
+// Scene
 	scene = new THREE.Scene();
-
+// Camera
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.set( 0, 0, 500 );
-
+// View Controller
 	controls = new THREE.TrackballControls( camera, renderer.domElement );
 	controls.minDistance = 200;
 	controls.maxDistance = 500;
 
 	scene.add( new THREE.AmbientLight( 0x222222 ) );
-
+// Lights
 	var light = new THREE.PointLight( 0xffffff );
 	light.position.copy( camera.position );
 	scene.add( light );
 
-	// create the core ball here
+// create the core ball here
 	coreBallShape = new THREE.IcosahedronGeometry(100, 1);
 	coreBallMaterial = new THREE.MeshBasicMaterial({color: 0x928852, wireframe: true})
-	coreBall = new THREE.Mesh(coreBallShape, coreBallMaterial);
+	coreBallShader = new THREE.ShaderMaterial(shaderTest);
+	coreBall = new THREE.Mesh(coreBallShape, coreBallShader);
 	scene.add(coreBall);
 	
-	// //spineline
-
-	// var closedSpline = new THREE.ClosedSplineCurve3( [
-	// 	new THREE.Vector3( -60, -100,  60 ),
-	// 	new THREE.Vector3( -60,   20,  60 ),
-	// 	new THREE.Vector3( -60,  120,  60 ),
-	// 	new THREE.Vector3(  60,   20, -60 ),
-	// 	new THREE.Vector3(  60, -100, -60 )
-	// ] );
-
-	// var extrudeSettings = {
-	// 	steps			: 100,
-	// 	bevelEnabled	: false,
-	// 	extrudePath		: closedSpline
-	// };
-
-
-	// var pts = [], count = 3;
-
-	// for ( var i = 0; i < count; i ++ ) {
-
-	// 	var l = 20;
-
-	// 	var a = 2 * i / count * Math.PI;
-
-	// 	pts.push( new THREE.Vector2 ( Math.cos( a ) * l, Math.sin( a ) * l ) );
-
-	// }
-
-	// var shape = new THREE.Shape( pts );
-
-	// var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-
-	// var material = new THREE.MeshLambertMaterial( { color: 0xb00000, wireframe: false } );
-
-	// var mesh = new THREE.Mesh( geometry, material );
-
-	// scene.add( mesh );
-
-
-	// //
-
-
-	// var randomPoints = [];
-
-	// for ( var i = 0; i < 10; i ++ ) {
-
-	// 	randomPoints.push( new THREE.Vector3( ( i - 4.5 ) * 50, THREE.Math.randFloat( - 50, 50 ), THREE.Math.randFloat( - 50, 50 ) ) );
-
-	// }
-
-	// var randomSpline =  new THREE.CatmullRomCurve3( randomPoints );
-
-	// //
-
-	// var extrudeSettings = {
-	// 	steps			: 200,
-	// 	bevelEnabled	: false,
-	// 	extrudePath		: randomSpline
-	// };
-
-
-	// var pts = [], numPts = 5;
-
-	// for ( var i = 0; i < numPts * 2; i ++ ) {
-
-	// 	var l = i % 2 == 1 ? 10 : 20;
-
-	// 	var a = i / numPts * Math.PI;
-
-	// 	pts.push( new THREE.Vector2 ( Math.cos( a ) * l, Math.sin( a ) * l ) );
-
-	// }
-
-	// var shape = new THREE.Shape( pts );
-
-	// var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-
-	// var material2 = new THREE.MeshLambertMaterial( { color: 0xff8000, wireframe: false } );
-
-	// var mesh = new THREE.Mesh( geometry, material2 );
-
-	// scene.add( mesh );
-
-
-	// //
-
-
-	// var materials = [ material, material2 ];
-
-	// var extrudeSettings = {
-	// 	amount			: 20,
-	// 	steps			: 1,
-	// 	material		: 1,
-	// 	extrudeMaterial : 0,
-	// 	bevelEnabled	: true,
-	// 	bevelThickness  : 2,
-	// 	bevelSize       : 4,
-	// 	bevelSegments   : 1,
-	// };
-
-	// var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-
-	// var mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
-
-	// mesh.position.set( 50, 100, 50 );
-
-	// scene.add( mesh );
 }
 
 function render(){
+	coreBall.rotation.x += 0.005;
+	coreBall.rotation.y += 0.005;
 	requestAnimationFrame( render );
 
 	controls.update();
